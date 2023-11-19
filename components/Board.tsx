5ef3e115845566746281c2d984430ee72460fe90
nodirbeksulaymonov <s.nodir9494@gmail.com>
2023-11-20 01:41:49 +0500

    add Board Component

--- components/Board.tsx
+++ components/Board.tsx
@@ -1,7 +1,26 @@
+'use client'
+
+import { useEffect } from 'react';
+import { DragDropContext, Droppable } from 'react-beautiful-dnd';
+
 function Board() {
+
+
+useEffect(()=> {
+
+}, [])
+
   return (
-    <div>Board</div>
+    <DragDropContext onDragEnd={Board}>
+        <Droppable droppableId='board' direction='horizontal' type='column'>
+            {(provided) => (
+                <div>
+                    {/* rednderiim */}
+                </div>
+            )}
+        </Droppable>
+    </DragDropContext>
   )
 }
 
-export default Board
+export default Board;
---
+++ store/BoardStore.tsx
@@ -0,0 +1,15 @@
+import { create } from 'zustand'
+
+interface BoardState {
+    board: Board;
+    getBoard: () => void;
+}
+
+const useBoardStore = create<BoardState>((set) => ({
+  board: {
+    columns: new Map<TypeColumn, Column>()
+  },
+  getBoard: async() => {
+    // const board = await getTodosGroupedColumn( )
+  }
+}))
