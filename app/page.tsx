a4e728b24eb9391f90959d73da1ab452b13058e6
nodirbeksulaymonov <s.nodir9494@gmail.com>
2023-11-19 23:16:15 +0500

    finish design header component

--- app/page.tsx
+++ app/page.tsx
@@ -1,10 +1,10 @@
 import Header from '@/components/Header';
-
+import Board from '@/components/Board';
 export default function Home() {
   return (
     <main>
       < Header />
-    <h1>Trello 2.0</h1>
+      < Board />
     </main>
   )
 }
