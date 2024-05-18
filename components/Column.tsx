2f4551bd84b091e6873c9ad477324728c6b505bd
nodirbeksulaymonov <s.nodir9494@gmail.com>
2024-05-19 02:13:27 +0500

    fix style Column component

--- components/Column.tsx
+++ components/Column.tsx
@@ -59,7 +59,7 @@ function Column({id, todos, index} : Props) {
                                     ))}
                                     {provided.placeholder}
                                     <div className="flex items-end justify-end p-2">
-                                        <button className="text-green-500 hover: text-green-600">
+                                        <button className="text-green-500 hover:text-green-600">
                                             <PlusCircleIcon className="h-10 w-10 " />
                                         </button>
                                     </div>
